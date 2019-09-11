6cdeecc531ec4cd04f63935ecd0c01e4d02a2614
jhwangit128 <peonyanko128@gmail.com>
2019-09-11 12:48:26 -0500

    updating app.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,77 @@
+// ++++++++++++++++++++++++++++++++++++
+// DEPENDENCIES
+// ++++++++++++++++++++++++++++++++++++
+// packages & components
 import React from 'react'
+import Header from './Header.js'
+import Main from './Main.js'
+import Footer from './Main.js'
 
+// ++++++++++++++++++++++++++++++++++++
+// COMPONENET CLASS
+// ++++++++++++++++++++++++++++++++++++
 class App extends React.Component {
+  // ++++++++++++
+  // STATE
+  // ++++++++++++
+  constructor(props) {
+    super(props)
+    this.state = {
+      view: {
+        page: 'index',
+        pageTitle: "Let's save the ocean"
+      },
+      formInputs: {
+        title: null,
+        username: null,
+        image: null,
+        body: null,
+        id: null
+      }
+    }
+  }
+
+  // ++++++++++++
+  // HANDLERS
+  // ++++++++++++
+  handleView = (view, tip) => {
+    let pageTitle = ''
+    let formInputs = {
+      title: '',
+      username: '',
+      image: '',
+      body: '',
+      id: null
+    }
+
+    switch (view) {
+      case 'index':
+        pageTitle = "Let's save the ocean"
+        break
+      case 'addTip':
+        pageTitle = 'Add a Tip'
+        break
+      case 'editTip':
+        pageTitle = 'Edit a Tip'
+        formInputs = {
+          title: tip.title,
+          username: tip.username,
+          image: tip.image,
+          body: tip.body,
+          id: tip.id
+        }
+        break
+      default:
+        break
+    }
+    this.setState({
+      view: {
+        page: view,
+        
+      }
+      }
+    })
+  }
   render () {
     return (
       <h1>Save The Ocean!</h1>
--- app/models/tip.rb
+++ app/models/tip.rb
@@ -15,7 +15,8 @@ class Tip
     return results.map do |result|
       {
         id: result["id"].to_i,
-        name: result["name"],
+        title: result["title"],
+        username: result["username"],
         image: result["image"],
         body: result["body"]
       }
@@ -23,4 +24,68 @@ class Tip
   end
 
   # show
-    def self.find(id)
+  def self.find(id)
+    results = DB.exec("SELECT * FROM tips WHERE id=#{id};")
+    if !results.num_tuples.zero?
+      return {
+        id: results.first["id"].to_i,
+        title: results.first["title"],
+        username: results.first["username"],
+        image: results.first["image"],
+        body: results.first["body"]
+      }
+    else
+      return {
+        "error" => "no such post, check the id!"
+      }, status: 400
+    end
+  end
+
+  # create
+  def self.create(opts)
+    results = DB.exec(
+      <<-SQL
+        INSERT INTO tips (title, username, image, body)
+        VALUES ('#{opts["title"]}', '#{opts["username"]}', '#{opts["image"]}', '#{opts["body"]}')
+        RETURNING id, title, username, image, body;
+      SQL
+    )
+    result = results.first
+    return {
+      id: result["id"].to_i,
+      title: result["title"],
+      username: result["username"],
+      image: result["image"],
+      body: result["body"]
+    }
+  end
+
+  def self.delete(id)
+    results = DB.exec("DELETE FROM tips WHERE id=#{id};")
+    return { "deleted" => true }
+  end
+
+  def self.update (id, opts)
+    results = DB.exec(
+      <<-SQL
+        UPDATE tips
+        SET
+          title='#{opts["title"]}',
+          username='#{opts["username"]}',
+          image='#{opts["image"]}',
+          body='#{opts["body"]}'
+        WHERE id=#{id}
+        RETURNING id, title, username, image, body;
+      SQL
+    )
+    result = results.first
+    return {
+      id: result["id"].to_i,
+      title: result["title"],
+      username: result["username"],
+      image: result["image"],
+      body: result["body"]
+    }
+  end
+
+end
