86941654fd051c55ad7e568343134df12f0545e0
jhwangit128 <peonyanko128@gmail.com>
2019-09-11 13:15:50 -0500

    updating main.js handlers

--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -26,6 +26,52 @@ class Main extends React.Component {
   fetchTips = () => {
     fetch('/tips')
       .then(data => data.json())
+      .then(jData => {
+        this.setState({ tips: jData })
+      })
+  }
+
+  handleCreate = (createTip) => {
+    fetch('/tips', {
+      body: JSON.stringify(createTip),
+      method: 'Post',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(createdTip => {
+        return createdTip.json()
+      })
+      .then(jsonedTip => {
+        this.setState(prevState => {
+          prevState.tips.push(jsonedTip)
+          return { tips: prevState.tips }
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  handleUpdate = (updateTip) => {
+    fetch(`tips/${updatedTip.id}`, {
+      body: JSON.stringify(updateTip),
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(updatedTip => {
+        this.props.handleView('index')
+        this.fetchTips()
+      })
+      .catch(err => console.log(err))
+  }
+
+  handleDelete = (id) => {
+    fetch(`tips/${id}`, {
+      method:
+    })
 
   }
 }
