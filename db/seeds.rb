22b7d607cba060813f4be9c318269f12dbece8df
jhwangit128 <peonyanko128@gmail.com>
2019-09-17 10:13:19 -0500

    seed data updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,71 +11,84 @@ Tip.create({
   "title" => "Carry reusable shopping bags",
   "username" =>"skylar11",
   "image" => "https://cdn11.bigcommerce.com/s-9geauonp44/images/stencil/1024x1024/products/536/1097/PPC-TOTE-NAT-LPF_CONTEXT_500X500__71258.1543257162.png?c=2",
+  "description" => "The usefulness of these thin and easily ripped bags is extremely limited, yet according to one estimate, somewhere between five billion and one trillion plastic bags are used each year around the world. Although free to shoppers, these bags have a high environmental cost and are one of the most ubiquitous forms of garbage. Bringing your own plastic bag is common but good environmental advice, such good advice that some governments implemented policies to encourage more people to do it. Disposable shopping bags have been banned in a number of cities, like San Francisco and Washington, D.C.
+  In addition to bigger carryall bags, you can further reduce waste by bringing your own reusable produce bags or skipping them entirely.",
   "img" => "https://7b7157f59fb5914df25d-83ec9bcc8970758aaa4b1923747e8d1b.ssl.cf1.rackcdn.com/b98de714e1c2dad2b5ad6b288865c259.jpg"
 })
 Tip.create({
   "title" => "Get reusable stainless steel bottles",
   "username" =>"snow3man5",
   "image" => "https://aswetravel.smugmug.com/Travel/GoGreenTravelGreen-2014/i-GxJhWH8/0/O/Insulated%20Stainless%20Steel%20Water%20Bottles.jpg",
+  "description" => "Not only does it come in a plastic bottle, but tremendous resources are used to extract, bottle, and ship it. And many brands of bottled water are simply filtered tap water. Get a reusable stainless steel bottle or stainless steel travel mug, fill it up with tap water before leaving the house, and refill it wherever you happen to be. I do not recommend reusable plastic or aluminum bottles. Plastic may leach chemicals into the water and aluminum bottles are lined with an epoxy resin, some of which has also found to leach into water depending on the brand. Why take a chance? Read my posts about bottled water for more information.",
   "img" => "https://www.businessgreen.com/w-images/b7c92aff-a2a4-4ea6-af49-b20b82379c89/2/creditJordiChiasPujol-580x358.png"
 })
 Tip.create({
-  "title" => "Carry your own containers for take-out food and leftovers",
+  "title" => "Re-think your food storage",
   "username" =>"shrimplin",
   "image" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0nmL0hxXLZ_M-2fPrM3pXBmWWiy-NhaV8Y3WX-I0Vm252gzba",
+  "description" => "Plastic baggies, plastic wrap, and plastic storage containers are worth re-evaluating. Instead of sandwich baggies, why not pack a bento box or a cute tiffin (shown at the top of this post) for lunch? Instead of throwing away plastic zipper bags or wrapping things in Saran wrap, why not use jars or glass containers in the fridge? When it comes to carryout, these types of containers be used instead of disposable ones—although it can definitely take a bit of courage and some explaining to help your local restaurants to understand.",
   "img" => "https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2018/09/931/524/24a1276c9561e230670f6a7067008a27.jpg?ve=1&tl=1"
 })
 Tip.create({
   "title" => "Carry reusable utensils and glass drinking straws",
   "username" =>"roseleaf",
   "image" => "https://thebestbrushes.com/images/products/8317.jpg",
+  "description" => "I keep a To-Go-Ware bamboo utensil set and a couple of GlassDharma drinking straws in my purse at all times. And actually, I didn’t need to go out and buy the bamboo. I could have just as easily used my own stainless steel utensils.",
   "img" => "https://transform.iema.net/sites/default/files/s3/hero/web_ocean_plastic_istock-1073416938.png"
 })
 Tip.create({
   "title" => "Shop your local farmers market",
   "username" =>"gaius",
   "image" => "https://icghomes.com/site/wp-content/uploads/2018/05/pexels-photo-95425-1024x682.jpeg",
+  "description" => "Farmers markets are a great way to buy fresh, local produce without plastic, as long as you remember to bring your own bags. Normally, the fruits and vegetables at farmers markets do not even have those little plastic stickers on them. And for small fruits like berries and cherry tomatoes, use your own container or bag and hand the vendor plastic container back to reuse.",
   "img" => "https://media2.s-nbcnews.com/i/MSNBC/Components/Video/__NEW/AP100215155274.jpg"
 })
 Tip.create({
   "title" => "Wash clothes with homemade laundry soap and stain removers",
   "username" =>"thorndale22",
   "image" => "https://livesimply.me/wp-content/uploads/2014/02/1-homemade-liguid-laundry-soap-all-natural-5-682x1024.jpg",
+  "description" => "Look for soap nuts in plastic-free packaging. Borax and Washing Soda come in cardboard boxes. Treat laundry stains with a borax/water paste or with a handmade laundry stain bar. Try the stain remover sticks from Juniperseed Mercantile or Buncha Farmers.",
   "img" => "https://www.nationalgeographic.com/content/dam/photography/PROOF/2018/June/animals-and-plastic/animals-plastic-nationalgeographic_1230842.jpg"
 })
 Tip.create({
   "title" => "Use a handkerchief instead of paper tissue",
   "username" =>"larixfda",
   "image" => "https://images.manmadediy.com/1VpkX4_bpKYatD6N_VYwB35VYTw=/800x0/filters:no_upscale()/https://assets.manmadediy.com/photos/33008/handkerchiefs-2original.jpg",
+  "description" => "I have never seen a Kleenex box without any plastic window. More importantly, we can avoid all waste by opting for reusable hankies. Some people make their own out of old t-shirts and cloth diapers. I found lots of hankies at a thrift shop. Another ingenious idea is the HankyBook, which makes carrying a cloth hanky so much neater.",
   "img" => "https://media.nature.com/w800/magazine-assets/d41586-019-01252-0/d41586-019-01252-0_16659420.jpg"
 })
 Tip.create({
   "title" => "Learn strategies for green gift giving",
   "username" =>"iberis09",
   "image" => "https://lobsterandswan.com/wp-content/uploads/2015/04/Urban-Jungle-Bloggers-April-2015-15.jpg",
+  "description" => "Give only what will be truly appreciated. Opt for experiences or services (like restaurant meals, tickets to events, your help with a task) over stuff. Also request zero plastic packaging when ordering online. I am trying to buy fewer things in general, but vendors do sometimes send me products to review for this blog. When that happens, I include a message to the seller requesting zero plastic or Styrofoam packaging, including plastic tape. (See my packaging policy here.) When this does not work, I’ve started to send back unwanted plastic packaging with a letter of explanation. And I send back unwanted plastic I receive unsolicited in the mail or on my doorstep.",
   "img" => "https://www.afdb.org/sites/default/files/styles/1700x900/public/csm_ocean-pollution-plastic_406064fb9c.jpg?itok=BCHef51_"
 })
 Tip.create({
   "title" => "Compost food waste to avoid plastic garbage bags",
   "username" =>"goldberry",
   "image" => "https://i.ytimg.com/vi/sWMwP8ciecM/hqdefault.jpg",
+  "description" => "I bought a 100% recycled plastic Urban Compost Tumbler and started composting. This solves several plastic problems. First, since we no longer put wet stuff in the garbage, we do not need plastic garbage bags of any kind and I can mix the compost with soil from the yard to pot my houseplants and avoid buying potting soil in plastic bags. Lately, though, I have not had the time or energy to maintain my compost bin. But here in Oakland(as well as Berkeley and San Francisco) we have city-wide composting. We can put all of our food scraps (including meat) and food-soiled paper, along with yard waste, into our green bins. It is then picked up with our garbage and taken to a commercial compost facility where our food scraps are converted into rich soil amendments for residents and local farms.",
   "img" => "https://static1.squarespace.com/static/57d2c17d29687f368b608a50/57d2ced0579fb3d5c11664cb/5b5fe8ac758d46a989ab4dd2/1535088448573/ocean-Plastic_eartha_banner.jpg?format=2500w"
 })
 Tip.create({
   "title" => "Throw a zero waste party",
   "username" =>"larixfda",
   "image" => "https://i.pinimg.com/originals/49/9c/a4/499ca4b73170c87e4f6841e5bdf2a97a.jpg",
+  "description" => "Provide durable dishes, glasses, utensils. Ask guests to bring their own dishes or at least cups. Stock up on thrift store utensils and mugs (mixing and matching crazy mugs can be fun) especially for parties. Request no plastic cling-wrap on potluck offerings. Ask guests to bring containers for leftovers, as they did at our Thanksgiving potluck.",
   "img" => "https://cdn.iflscience.com/images/a7ca41fb-1ea2-5c1c-8b57-7ede1b6d09f4/default-1521631774-cover-image.jpg"
 })
 Tip.create({
   "title" => "Opt for a real, sustainably-grown and harvested tree, a live tree that can be planted, or an artificial tree made from natural materials",
   "username" =>"thorndale22",
   "image" => "https://easytreezy.com/wp-content/uploads/2018/09/easy-treezy-natural-pre-lit-artificial-christmas-tree-led-lights-510x510.jpg",
+  "description" => "Most artificial trees are made from toxic PVC. Opt for a real, sustainably-grown and harvested tree, a live tree that can be planted, or an artificial tree made from natural materials. There are trees made from recycled cardboard, wood, or even recycled glass bottles.",
   "img" => "https://www.wwf.org.au/images/useruploadedimages/676/iload3911___medium.jpg"
 })
 Tip.create({
   "title" => "Get off mailing lists to avoid plastic envelope windows",
   "username" =>"skylar11",
   "image" => "https://img-aws.ehowcdn.com/750x428p/photos.demandstudios.com/getty/article/139/127/139908569.jpg",
+  "description" => "I have switched to online billing and online statements; canceled subscriptions; and called to have my name removed from mailing lists. I want to save paper as well as plastic. Catalog Choice can help. (Naturally, I am trying to save not just the windows but the paper and all the energy to from delivery as well!)",
   "img" => "https://static.standard.co.uk/s3fs-public/thumbnails/image/2019/01/30/16/grey-seal-pup-plastic-credit-matt-carter.jpg?width=1368&height=912&fit=bounds&format=pjpg&auto=webp&quality=70"
 })
